78c796579c420fa33f5215b5eb772bb9d6c22244
Pravallika <pkathi@usf.edu>
2025-09-20 00:28:32 -0400

    discount

--- cc_4.js
+++ cc_4.js
@@ -5,3 +5,27 @@ let products = [
     {SKU: "PR4", name: "Lamp", category: "Household", price: 15, inventory: "200"},
     {SKU: "PR5", name: "Jeans", category: "Apparel", price: 60, inventory: "150"},
 ]
+
+let discount = 0;
+for (const product of products) {
+    switch (product.category) {
+        case "Electronics":
+            discount = 0.2;
+            break;
+        case "Apparel":
+            discount = 0.15;
+            break;
+        case "Groceries":
+        case "Household":
+            discount = 0.1;
+            break;
+        default:
+            discount = 0;
+            break;
+    }
+    // Apply discount to the product 
+let promoPrice = product.price * (1-discount);
+product.promoPrice = promoPrice;
+}
+
+console.log(products)
