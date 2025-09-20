8d32def82cc71630c6b31425d6896ed90792f759
Pravallika <pkathi@usf.edu>
2025-09-19 23:23:44 -0400

    products

---
+++ cc_4.js
@@ -0,0 +1,7 @@
+let products = [
+    {SKU: "PR1", name: "Phone", category: "Electronics", price: 800, inventory: "50"},
+    {SKU: "PR2", name: "Shirt", category: "Apparel", price: 20, inventory: "100"},
+    {SKU: "PR3", name: "Potatos", category: "Groceries", price: 15, inventory: "30"},
+    {SKU: "PR4", name: "Lamp", category: "Household", price: 15, inventory: "200"},
+    {SKU: "PR5", name: "Jeans", category: "Apparel", price: 60, inventory: "150"},
+]
